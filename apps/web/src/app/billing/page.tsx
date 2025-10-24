a375bf77a976bb1f005458e6773e0fc7e0d940f4
Chase Shooltz <chaseshooltz@Chases-MacBook-Pro.local>
2025-10-24 16:29:30 -0400

    sec(billing): require auth + default org; idempotent Stripe webhooks; remove auto-create org

--- apps/web/src/app/billing/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import UpgradeModal from './upgrade-modal'
-export default function BillingPage() {
-  return (
-    <main className="min-h-screen grid place-items-center p-8">
-      <UpgradeModal />
-    </main>
-  )
-}
